48a4d08b6eac1f686a8efd450476089745e7f2d7
Virat-Shrimali <viratns05@gmail.com>
2026-05-23 15:53:55 +0000

    Deploying to gh-pages from @ Virat-Shrimali/Virat-Shrimali.github.io@699ebba001f5a279fe8aa1ca31f109ba447329e4 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Virat-Shrimali.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://virat-shrimali.github.io/#contact">contact us</a>.'
               }
             ]
           }
